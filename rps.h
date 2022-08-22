ac677008ed447bb59dba12ebd0d26b246c65ce6f
theliamdavidson <misterliamd@gmail.com>
2022-08-22 14:13:59 -0700

    ah

---
+++ rps.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#ifndef rps_H
+#define rps_H
+using namespace std;
+class Game
+{
+    private:
+        int Win_check(char in1, char in2){
+            
+        }
+};
+class Player
+{
+    private:
+        char move;
+    public: 
+        Player(){
+            cout << "enter your move: (R, P, or S) \n";
+            cin >> move;
+        }
+};
+
+#endif
---
+++ rps_class.cpp
@@ -0,0 +1,15 @@
+#include "rps.h"
+
+using namespace std;
+
+int main(){
+    Game rps;
+    cout << "welcome to rps v2.0 \n";
+    while(1){
+        cout << "Player 1, ";
+        Player p1;
+        cout << "Player 2, ";
+        Player p2;
+        
+    }
+}
