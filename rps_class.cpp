6feced64a266cd34e6d2926192c106fdf1e33933
theliamdavidson <misterliamd@gmail.com>
2022-08-22 16:00:37 -0700

    functional, missing complete input cleaner

--- rps_class.cpp
+++ rps_class.cpp
@@ -5,13 +5,11 @@ using namespace std;
 int main(){
     Game rps;
     cout << "welcome to rps v2.0 \n";
-    while(1){
+    while(rps.game_run){
         cout << "Player 1, ";
         Player p1;
         cout << "Player 2, ";
         Player p2;
-        
+        rps.win_statement(rps.win_check(p1.move, p2.move));
     }
 }
-//                case 'i':
-//                    return 100;
